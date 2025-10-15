97a4931e7439d7ecc6b5c6221c57f66d2b52d6fb
KhalilKes <akakhalilyo@gmail.com>
2025-10-15 11:41:13 +0100

    change connection string

--- src/main/java/com/ofppt/products_api/controller/ProductController.java
+++ src/main/java/com/ofppt/products_api/controller/ProductController.java
@@ -2,12 +2,14 @@ package com.ofppt.products_api.controller;
 
 import com.ofppt.products_api.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ofppt.products_api.repo.ProductRepo;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 public class ProductController {
     @Autowired
